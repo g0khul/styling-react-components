23d3e413dd09b03262702a0f1617b643792cb22d
gokhul <84320534+g0khul@users.noreply.github.com>
2026-01-17 00:00:22 +0530

    vanilla css, styled components

---
+++ src/components/Button.jsx
@@ -0,0 +1,30 @@
+import { styled } from 'styled-components';
+
+export const FilledButton = styled.button`
+  padding: 1rem 2rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 0.25rem;
+  color: #1f2937;
+  background-color: #f0b322;
+  border-radius: 6px;
+  border: none;
+
+  &:hover {
+    background-color: #f0920e;
+  }
+`;
+
+export const TextButton = styled.button`
+  color: #f0b322;
+  border: none;
+
+  & {
+    color: #f0b322;
+    border: none;
+  }
+
+  &:hover {
+    color: #f0920e;
+  }
+`
